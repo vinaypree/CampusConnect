bcd7874b73a077012046747e219de34df0291226
2025aim1005 <2025aim1005@iitrpr.ac.in>
2025-10-26 19:18:12 +0530

    Comment

---
+++ app/src/main/java/com/yourname/campusconnect/data/models/Comment.kt
@@ -0,0 +1,12 @@
+package com.yourname.campusconnect.data.models
+
+import com.google.firebase.Timestamp
+
+data class Comment(
+    val commentId: String = "",
+    val postId: String = "",
+    val authorId: String = "",
+    val authorName: String = "",
+    val text: String = "",
+    val timestamp: Timestamp = Timestamp.now()
+)
